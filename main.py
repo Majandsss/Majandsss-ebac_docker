fcdcce9166a2baa43c694a604ebebbd587f62eab
Majari <majaandressa@hotmail.com>
2026-05-21 20:58:03 -0300

    implementando redis

--- main.py
+++ main.py
@@ -1,19 +1,24 @@
+import asyncio
 from fastapi import FastAPI, HTTPException, Depends, status
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 from pydantic import BaseModel
 from typing import Optional
 import secrets
 import os
+import redis
+import json
 
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./livros.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+redis_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
 
 app = FastAPI(
     title="API de Livros",
@@ -25,9 +30,8 @@ app = FastAPI(
     }
 )
 
-MEU_USUARIO = os.getenv("MEU_USUARIO")
-MINHA_SENHA = os.getenv("MINHA_SENHA")
-
+MEU_USUARIO = os.getenv("MEU_USUARIO", "admin")
+MINHA_SENHA = os.getenv("MINHA_SENHA", "1234")
 security = HTTPBasic()
 
 def autenticar_meu_usuario(credentials: HTTPBasicCredentials = Depends(security)):
@@ -57,6 +61,12 @@ class Livro(BaseModel):
 
 Base.metadata.create_all(bind=engine)
 
+def salvar_livro_redis(livro_id: int, livro: Livro):
+    redis_client.set(f"livro:{livro_id}", json.dumps(livro.model_dump()))
+
+def deletar_livro_redis(livro_id: int):
+    redis_client.delete(f"livro:{livro_id}")
+
 def sessao_db():
     db = SessionLocal()
     try:
@@ -69,27 +79,72 @@ def sessao_db():
 def hello_world():
     return {"Hello": "World!"}
 
+@app.get("/debug/redis")
+def ver_livros_redis():
+    chaves = redis_client.keys("livros:*")
+    livros = []
+
+    for chave in chaves:
+        valor = redis_client.get(chave)
+        ttl = redis_client.ttl(chave)
+
+        livros.append({"chave": chave, "valor": json.loads(valor), "ttl":ttl})
+
+    return livros
+
 @app.get("/livros")
-def get_livros(page: int = 1, limit: int = 10, db: Session = Depends(sessao_db), usuario: str = Depends(autenticar_meu_usuario)):
+def get_livros(
+    page: int = 1,
+    limit: int = 10,
+    db: Session = Depends(sessao_db),
+    credentials: HTTPBasicCredentials = Depends(autenticar_meu_usuario)
+):
     if page < 1 or limit < 1:
-        raise HTTPException(status_code=400, detail="Page or limit inválidos!")
+        raise HTTPException(status_code=400, detail="Page or limit inválidos")
+    
+    cache_key = f"livros:page={page}&limit={limit}"
+    cached = redis_client.get(cache_key)
+
+    if cached:
+        return json.loads(cached)
     
     livros = db.query(LivroDB).offset((page - 1) * limit).limit(limit).all()
+
+    if not livros:
+        return {"message": "Não existe livro nenhum!"}
+    
     total_livros = db.query(LivroDB).count()
 
-    if total_livros == 0:
-        return {"message": "Não existe nenhum livro!", "livros": []}
-
-    return {
+    resposta = {
         "page": page,
         "limit": limit,
-        "usuario_ativo": usuario,
         "total": total_livros,
-        "livros":[{"id": livro.id, "nome_livro": livro.nome_livro, "autor_livro": livro.autor_livro, "ano_livro": livro.ano_livro} for livro in livros]
+        "livros": [
+            {
+                "id": livro.id,
+                "nome_livro": livro.nome_livro,
+                "autor_livro": livro.autor_livro,
+                "ano_livro": livro.ano_livro
+            } for livro in livros
+        ]
     }
+
+    redis_client.setex(cache_key, 30, json.dumps(resposta))
+
+    return resposta
     
 @app.post("/adiciona")
-def post_livros(livro: Livro, db: Session = Depends(sessao_db), usuario: str = Depends(autenticar_meu_usuario)):
+async def post_livros(livro: Livro, db: Session = Depends(sessao_db), usuario: str = Depends(autenticar_meu_usuario)):
+    # 💡 ADICIONADO O .first() NO FINAL DA CONSULTA
+    db_livro = db.query(LivroDB).filter(
+        LivroDB.nome_livro == livro.nome_livro, 
+        LivroDB.autor_livro == livro.autor_livro, 
+        LivroDB.ano_livro == livro.ano_livro
+    ).first()
+    
+    if db_livro:
+        raise HTTPException(status_code=400, detail="Esse livro já existe aqui!")
+    
     novo_livro = LivroDB(
         nome_livro=livro.nome_livro,
         autor_livro=livro.autor_livro,
@@ -100,31 +155,38 @@ def post_livros(livro: Livro, db: Session = Depends(sessao_db), usuario: str = D
     db.commit()
     db.refresh(novo_livro)
     
+    # Salva no cache do Redis usando o ID gerado pelo banco
+    salvar_livro_redis(novo_livro.id, livro)
+
     return {"message": f"Livro '{livro.nome_livro}' adicionado com o ID {novo_livro.id} por {usuario}!"}    
-    
+
 @app.put("/atualiza/{id_livro}")
-def put_livros(id_livro: int, livro: Livro, db: Session = Depends(sessao_db), usuario: str = Depends(autenticar_meu_usuario)):
-    livro_db = db.query(LivroDB).filter(LivroDB.id == id_livro).first()
-    
-    if not livro_db:
+async def put_livros(id_livro: int, livro: Livro, db: Session = Depends(sessao_db), usuario: str = Depends(autenticar_meu_usuario)):
+    db_livro = db.query(LivroDB).filter(LivroDB.id == id_livro).first()   
+    if not db_livro:
         raise HTTPException(status_code=404, detail="Esse livro não foi encontrado!")
     
-    livro_db.nome_livro = livro.nome_livro
-    livro_db.autor_livro = livro.autor_livro
-    livro_db.ano_livro = livro.ano_livro
+    db_livro.nome_livro = livro.nome_livro
+    db_livro.autor_livro = livro.autor_livro
+    db_livro.ano_livro = livro.ano_livro
         
     db.commit()
+    db.refresh(db_livro)
+
+    salvar_livro_redis(db_livro.id, livro)
 
     return {"message": f"Livro atualizado com sucesso por {usuario}!"}
 
 @app.delete("/deletar/{id_livro}")
-def delete_livro(id_livro: int, db: Session = Depends(sessao_db), usuario: str = Depends(autenticar_meu_usuario)):
-    livro_db = db.query(LivroDB).filter(LivroDB.id == id_livro).first()
+async def delete_livro(id_livro: int, db: Session = Depends(sessao_db), usuario: str = Depends(autenticar_meu_usuario)):
+    db_livro = db.query(LivroDB).filter(LivroDB.id == id_livro).first()
 
-    if not livro_db:
+    if not db_livro:
         raise HTTPException(status_code=404, detail="Esse livro não foi encontrado!")
     
-    db.delete(livro_db)
+    db.delete(db_livro)
     db.commit()
+
+    deletar_livro_redis(id_livro)
 
     return {"message": f"Livro deletado com sucesso por {usuario}!"}
